548a743467366644627d508160f19eccd602c3da
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-09-23 14:00:39 +0400

    Create main.php

---
+++ OneHitKillHG/main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OneHitKillHG;
+
+use pocketmine\player;
+use pocketmine\pluginbase;
+
+class main extends PluginBase{
+
+public function onEnable($this){
+$this->getLogger("[HG]HungerGames plugin loaded, latest version MCPE 0.12.1 alpha, PocketMine v1.6")
+
+>

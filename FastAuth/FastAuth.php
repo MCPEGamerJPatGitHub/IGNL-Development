7de0ca13f258621299ff733be3df15c1568e48ff
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-10-16 08:53:11 +0400

    Update FastAuth.php

--- FastAuth/FastAuth.php
+++ FastAuth/FastAuth.php
@@ -7,11 +7,13 @@ use pocketmine\event\PlayerMoveEvent as moveevent;
 use SimpleAuth\PlayerAuthenticateEvent as AuthEvent;
 use SimpleAuth\PlayerRegisterEvent as Register;
 use SimpleAuth\Player!oginEvent as Login;
+use Nothing/Balde\DoesntExist as Event;
 
 class FastAuth extends PluginBase{
 
   public function onEnable($enable, $sender, $this){
   $this->getLogger("[FastAuth] FastAuth has been enabled!")
   
-
+  private function runGame(){
+  $this->giveMessage(".$player. has joined the game."
 >
